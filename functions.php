831798b86241d9b31b9c850d73b484470f78375e
Evan McMillin <144277482+emcmillin25@users.noreply.github.com>
2023-09-07 17:10:43 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,16 +1,13 @@
 <?php
-
-
-
-?>function memberAge($birthday){
+function memberAge($birthday){
 	$birthday = date('Y-m-d', strtotime($birthday));
 	$today = date('Y-m-d');
 	$years = date_diff(date_create($birthday), date_create($today));
 	return $years->format('%y');
 }
-
 foreach($teammates as $team){
 $birthday = $team['birthday'];
 $age = memberAge($birthday);
 $ages[$team['name']] = $age;
 }
+?>
