118c6568bed89bdf9995d83672c5cac685bd377a
Evan McMillin <144277482+emcmillin25@users.noreply.github.com>
2023-09-07 17:08:41 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,22 +1,16 @@
 <?php
 
-/*
-Member age
-- create a function that calculates the team member's age based on their date of birth (parameter) and current date. Display the age both on the index and detail pages.
 
 
-*/
+?>function memberAge($birthday){
+	$birthday = date('Y-m-d', strtotime($birthday));
+	$today = date('Y-m-d');
+	$years = date_diff(date_create($birthday), date_create($today));
+	return $years->format('%y');
+}
 
-
-/*
-Card on the index page: create a function that displays each member's information on the index page. The function accepts the member's information (array) and the index of the member (number) as parameters, and it must display the code for the card filled with the member's information.
-
-
-*/
-/*
-Work experiences on the detail page: create a function that displays each work experience item on the detail page. The function accepts one work experience item as a parameter, and it must display the code for the work experience filled with the employment information.
-
-*/
-
-
-?>
+foreach($teammates as $team){
+$birthday = $team['birthday'];
+$age = memberAge($birthday);
+$ages[$team['name']] = $age;
+}
